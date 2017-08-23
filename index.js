1161bb54aa748ef9c346a30db3e11aef26e1de79
nick spragg <nick.spragg@bbc.co.uk>
2017-08-23 10:38:55 +0100

    Feature: Export basic logger middleware.

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const DEFAULT_TRANSPORT = require('./lib/transport/request');
 module.exports.transport = require('./lib/transport/transport');
 module.exports.context = require('./lib/context');
 module.exports.toJson = require('./lib/plugins/asJson');
+module.exports.logger = require('./lib/plugins/logger');
 
 module.exports.createClient = (httpTransport) => new Client(httpTransport || this.getDefaultTransport());
 
