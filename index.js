3669ceaebd5df3a758aa20478e1d73ee8f338bc8
nick spragg <nick.spragg@bbc.co.uk>
2017-08-25 09:21:25 +0100

    Task: Export default transport.

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ const Client = require('./lib/client');
 const DEFAULT_TRANSPORT = require('./lib/transport/request');
 
 module.exports.transport = require('./lib/transport/transport');
+module.exports.defaultTransport = require('./lib/transport/request');
 module.exports.context = require('./lib/context');
 module.exports.toJson = require('./lib/plugins/asJson');
 module.exports.logger = require('./lib/plugins/logger');
 
-module.exports.createClient = (httpTransport) => new Client(httpTransport || this.getDefaultTransport());
-
-module.exports.getDefaultTransport = () => {
-  return new DEFAULT_TRANSPORT;
-};
+module.exports.createClient = (httpTransport) => new Client(httpTransport || new DEFAULT_TRANSPORT);
