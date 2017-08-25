1b7a9a666b1106aa6150f83da7686e9302982eb5
nick spragg <nick.spragg@bbc.co.uk>
2017-08-25 15:07:49 +0100

    Task: Ignore empty objects to allow more method chaining.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const Client = require('./lib/client');
-const DEFAULT_TRANSPORT = require('./lib/transport/request');
+
+const DefaultTransport = require('./lib/transport/request');
 
 module.exports.transport = require('./lib/transport/transport');
-module.exports.defaultTransport = require('./lib/transport/request');
+module.exports.defaultTransport = DefaultTransport;
 module.exports.context = require('./lib/context');
 module.exports.toJson = require('./lib/plugins/asJson');
 module.exports.logger = require('./lib/plugins/logger');
 
-module.exports.createClient = (httpTransport) => new Client(httpTransport || new DEFAULT_TRANSPORT);
+module.exports.createClient = (httpTransport) => new Client(httpTransport || new DefaultTransport);
