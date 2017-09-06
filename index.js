d0e52c3ec045103a2fd32fb6183e1386e8d1703a
nick spragg <nick.spragg@bbc.co.uk>
2017-09-06 14:00:24 +0100

    Task: Export middleware to modify context.

--- index.js
+++ index.js
@@ -9,5 +9,6 @@ module.exports.defaultTransport = DefaultTransport;
 module.exports.context = require('./lib/context');
 module.exports.toJson = require('./lib/plugins/asJson');
 module.exports.logger = require('./lib/plugins/logger');
+module.exports.setContextProperty = require('./lib/plugins/setContextProperty');
 
 module.exports.createClient = (httpTransport) => new Client(httpTransport || new DefaultTransport);
