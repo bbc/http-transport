f6cef564ad91ab3a4a42086a0f87305956769e45
Nik Rahmel <nik@rahmel.eu>
2018-04-23 16:37:52 +0100

    Fix: asJson ignores invalid bodies

--- test/middlware/asJson.js
+++ test/middlware/asJson.js
@@ -95,6 +95,25 @@ describe('asJson', () => {
     assert.deepEqual(ctxWithJsonBody.res.body, {});
   });
 
+  it('does not parse the response if there is no body', async () => {
+    sandbox.stub(JSON, 'parse').throws();
+
+    const asJson = asJsonPlugin();
+
+    const ctxWithJsonBody = {
+      res: {
+        body: undefined,
+        headers: {
+          'content-type': 'application/json'
+        }
+      }
+    };
+
+    await asJson(ctxWithJsonBody, stubPromise());
+    sinon.assert.notCalled(JSON.parse);
+    assert.deepEqual(ctxWithJsonBody.res.body, undefined);
+  });
+
   it('throws an error when throw property is set true', async () => {
     const asJson = asJsonPlugin({
       throwOnConflict: true
